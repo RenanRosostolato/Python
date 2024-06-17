ac38d1ed4a68a114d1ab637799e854afc9a4b798
Renan Rosostolato <renan91@live.com>
2024-06-17 02:30:36 -0300

    Update variaveris_constantes.py

--- DIO/variaveris_constantes.py
+++ DIO/variaveris_constantes.py
@@ -2,4 +2,13 @@ nome = "Guilherme"
 
 idade = 28
 
+nome2, idade2 = "Giovana", 27
+
 print(nome, idade)
+print(nome2, idade2)
+
+limite_saqu_diario = 1000
+
+STATES = [ "MJ", "RJ","SC", "RS"]
+
+print(STATES)
