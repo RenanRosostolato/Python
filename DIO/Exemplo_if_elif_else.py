5f856f81535941d9284d80994b65e575d9d5b912
Renan Rosostolato <renan91@live.com>
2024-07-01 01:57:36 -0300

    estruturas condicional / if aninhado

---
+++ DIO/Exemplo_if_elif_else.py
@@ -0,0 +1,12 @@
+import sys
+
+opcao = int (input("informe uma opção: [1] sacar \n [2] Extrato: "))
+
+if opcao == 1:
+    valor =float (input("Informe a quantia para o saque:"))
+    #...
+elif opcao == 2:
+    print("Extrato:")
+
+else:
+    sys.exit ("Opção Invalida")
