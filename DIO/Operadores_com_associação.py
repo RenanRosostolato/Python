a24870f930308fa9f5b8d726999b80b644cec735
Renan Rosostolato <renan91@live.com>
2024-07-01 00:29:11 -0300

    Novos exercícios.

---
+++ DIO/Operadores_com_associação.py
@@ -0,0 +1,18 @@
+Frutas = [ "limão", "Uva"]
+curso = "Curso de Python"
+
+
+print("laranja" in Frutas)
+
+print("laranja" not in Frutas)
+
+print("limão" in Frutas)
+
+print("limão" not in Frutas)
+
+print("Python" in curso)
+
+print("Curso" not in curso)
+
+x = (22 - 10) * 3
+print(x)
