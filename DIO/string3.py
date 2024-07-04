3531021d5c3b31fbe9e71492a57bea7a7ce3ff95
Renan Rosostolato <renan91@live.com>
2024-07-04 20:27:44 -0300

    Update string3.py

--- DIO/string3.py
+++ DIO/string3.py
@@ -1,9 +1,29 @@
-# TIRANDO ESPAÇOS EM BRANCO !
+nome = " Renan Galdino Rosostolato"
 
-curso = "  python "
+print (
+    nome[0],
 
-print(curso.strip())
+    nome[:9],
+    nome[10:],
 
-print (curso.lstrip())
+    nome[10:16],
 
-print(curso.rstrip())
+    nome[10:16:2],
+
+    nome[:],
+
+    nome[::-1], 
+
+    sep='\n')
+
+
+#nome = "Renan Galdino Rosostolato"
+
+#print("nome[0]:", nome[0])
+#print("nome[:9]:", nome[:9])
+#print("nome[10:]:", nome[10:])
+#print("nome[10:16]:", nome[10:16])
+#print("nome[10:16:2]:", nome[10:16:2])
+#print("nome[:]:", nome[:])
+#print("nome[::-1]:", nome[::-1])
+
