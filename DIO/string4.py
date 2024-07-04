4a24df5b3e9b86aff087bf53c7d7cfc6814f7a1c
Renan Rosostolato <renan91@live.com>
2024-07-04 20:48:08 -0300

    adicionado string4

---
+++ DIO/string4.py
@@ -0,0 +1,19 @@
+nome = "Renan"
+
+mensagem = f"""
+    Olá meu nome é {nome}
+Eu estou aprendendo Python;
+        Essa mensagem tem diferentes recuos.
+"""
+print(mensagem)
+
+
+print("""
+        ============== Menu ================
+
+        1 - deposistar
+        2 - sacar
+        0 - sair
+
+            Obrigado por usar nossso sistema !!!
+    """)
